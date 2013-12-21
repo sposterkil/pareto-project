9a7065c6cd69b432032a9542449587158aa50a19
Sam Osterkil <pisketch@gmail.com>
2013-12-21 14:30:02 -0600

    Add definable # of header rows

--- paretoer.py
+++ paretoer.py
@@ -13,10 +13,11 @@ class CSVParetoer():
     of tags to be applied to that file.
     """
 
-    def __init__(self, input_path, output_path):
+    def __init__(self, start_row, input_path, output_path):
         self.input_file = open(input_path, "rb")
         self.output_file = open(output_path, "wb")
         self.stop_words = open("stopwords.txt", "r").read().split()
+        self.start_row = int(start_row)
         self.counts = {}
 
     def update_counts(self, str_to_count):
@@ -34,6 +35,8 @@ class CSVParetoer():
         dialect = csv.Sniffer().sniff(self.input_file.read(1024))
         self.input_file.seek(0)
         reader = csv.reader(self.input_file, dialect)
+        for i in xrange(0, self.start_row):
+            next(reader)
         for line in reader:
             for number in column_list:
                 self.update_counts(line[number])
@@ -48,12 +51,18 @@ class CSVParetoer():
 if __name__ == '__main__':
     input_path = sys.argv[1]
     output_path = sys.argv[2]
+    num_header_rows = raw_input(
+        "How many rows should we exclude as Header rows?\n")
+    if num_header_rows >= 1:
+        title_row = raw_input(
+            "Which of these rows contains the column titles?\n")
     column_string = raw_input(
         'Which columns would you like to read from? Numbers separated by commas, please.\n')
+
     column_list = [int(x) - 1 for x in column_string.split(",")]
     # TODO: Column names! Let us pick them, or at least say which we're using
     print "Opening files..."
-    paretoer = CSVParetoer(input_path, output_path)
+    paretoer = CSVParetoer(num_header_rows, input_path, output_path)
     print "Counting..."
     paretoer.pareto(column_list)
     print 'Writing output...'

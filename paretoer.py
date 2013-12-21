b2f7046c463fd147e828e4ca2ccc3978445b1194
Sam Osterkil <pisketch@gmail.com>
2013-12-21 13:02:13 -0600

    Syntax derps

--- paretoer.py
+++ paretoer.py
@@ -18,44 +18,44 @@ class CSVParetoer():
         self.output_file = open(output_path, "w")
         self.counts = {}
 
-        def update_counts(self, str_to_count):
-            zero_punctuation = str_to_count.translate(None, string.punctuation)
-            word_list = zero_punctuation.split()
-            for word in word_list:
-                word = word.upper()
-                if word in self.counts:
-                    self.counts[word] += 1
-                else:
-                    self.counts[word] = 1
-
-        def pareto(self, column_list):
-            dialect = csv.Sniffer().sniff(self.input_file.read(1024))
-            input_file.seek(0)
-            reader = csv.reader(csvfile, dialect)
-            if csv.Sniffer().has_header(self.input_file.read(1024)):
-                print "Reading from these columns:"
-                for number in column_list:
-                    print reader.next()[number]
+    def update_counts(self, str_to_count):
+        zero_punctuation = str_to_count.translate(None, string.punctuation)
+        word_list = zero_punctuation.split()
+        for word in word_list:
+            word = word.upper()
+            if word in self.counts:
+                self.counts[word] += 1
             else:
-                reader.next()
+                self.counts[word] = 1
 
-            for line in reader:
-                for number in column_list:
-                    update_counts(line[number])
+    def pareto(self, column_list):
+        dialect = csv.Sniffer().sniff(self.input_file.read(1024))
+        self.input_file.seek(0)
+        reader = csv.reader(self.input_file, dialect)
+        if csv.Sniffer().has_header(self.input_file.read(1024)):
+            print "Reading from these columns:"
+            for number in column_list:
+                print reader.next()[number]
+        else:
+            reader.next()
 
-        def write_counts(self):
-            count_tuples = self.counts.iteritems()
-            sorted_tuples = sorted(count_tuples, key=lambda word: word[1])
-            for word, count in sorted_tuples:
-                self.output_file.write("%s: %s" % word, count)
-                self.output_file.write("\n")
+        for line in reader:
+            for number in column_list:
+                self.update_counts(line[number])
+
+    def write_counts(self):
+        count_tuples = self.counts.iteritems()
+        sorted_tuples = sorted(count_tuples, key=lambda word: -word[1])
+        for word, count in sorted_tuples:
+            self.output_file.write("%s: %s" % (word, count))
+            self.output_file.write("\n")
 
 if __name__ == '__main__':
     input_path = sys.argv[1]
     output_path = sys.argv[2]
-    column_string = input(
+    column_string = raw_input(
         'Which columns would you like to read from? Numbers separated by commas, please.\n')
-    column_list = int(x) for x in column_string.split(",")
+    column_list = [int(x) - 1 for x in column_string.split(",")]
     paretoer = CSVParetoer(input_path, output_path)
     paretoer.pareto(column_list)
     paretoer.write_counts()

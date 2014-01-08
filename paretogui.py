7055c06f457e23034cc747317c6875d840f16b2b
Sam Osterkil <pisketch@gmail.com>
2014-01-07 22:35:25 -0600

    Finished GUI functionality

--- paretogui.py
+++ paretogui.py
@@ -2,9 +2,11 @@
 
 import csv
 import os
+import shutil
 import Tkconstants
 import tkFileDialog
 import Tkinter as Tk
+import webbrowser
 from paretoer import CSVParetoer, CSVTagger
 
 
@@ -56,42 +58,44 @@ class TkinterGUI(Tk.Frame):
         options['parent'] = root
         options['title'] = 'Choose A Directory'
 
-    def tag_file(self):
-        pass
-
     def update_header_chooser(self, items_to_add):
+        self.header_chooser.delete(0, Tk.END)
         for item in items_to_add:
             self.header_chooser.insert(Tk.END, item)
 
     def choose_file(self):
-        """Returns an opened file in read mode."""
+        try:
+            self.edit_msg.pack_forget()
+        except Exception, e:
+            pass
         self.file_to_tag = tkFileDialog.askopenfile(mode='r', **self.file_opt)
         headers = next(csv.reader(self.file_to_tag))
         self.file_to_tag.seek(0)
         self.update_header_chooser(headers)
-        self.open_button.config(state=Tk.DISABLED)
+        shutil.copyfile(self.file_to_tag.name, self.file_to_tag.name + ".BAK")
         self.count_button.config(state=Tk.NORMAL)
 
     def start_pareto(self):
-        self.tag_file = open(os.path.join(get_parent_dir(self.file_to_tag),
-                                          "tagfile.txt"), "w")
-        self.counter = CSVParetoer(1, self.file_to_tag, self.tag_file)
-        column_list = map(int, self.header_chooser.curselection())
-        self.counter.pareto(column_list)
+        self.tag_out = open(os.path.join(get_parent_dir(self.file_to_tag),
+                                         "tagfile.txt"), "w")
+        self.counter = CSVParetoer(1, self.file_to_tag, self.tag_out)
+        self.column_list = map(int, self.header_chooser.curselection())
+        self.counter.pareto(self.column_list)
         self.counter.write_counts()
         self.count_button.config(state=Tk.DISABLED)
         self.tag_button.config(state=Tk.NORMAL)
-        print "Please edit tagfile.txt in the same directory as the chosen csv file to select your tags."
+        self.edit_msg = Tk.Message(self,
+                                   text="Please edit tagfile.txt in the same directory as the chosen csv file to select your tags.")
+        self.edit_msg.pack()
+        webbrowser.open(self.tag_out.name)
 
-    def asksaveasfile(self):
-        """Returns an opened file in write mode."""
-
-        return tkFileDialog.asksaveasfile(mode='w', **self.file_opt)
-
-    def askdirectory(self):
-        """Returns a selected directoryname."""
-
-        return tkFileDialog.askdirectory(**self.dir_opt)
+    def tag_file(self):
+        self.edit_msg.pack_forget()
+        tag_in = open(self.tag_out.name)
+        self.tag_out.close()
+        tagger = CSVTagger(self.column_list, tag_in, self.file_to_tag)
+        tagger.add_tags()
+        self.tag_button.config(state=Tk.DISABLED)
 
 
 if __name__ == '__main__':
